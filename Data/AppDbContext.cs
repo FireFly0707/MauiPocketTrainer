67f26d1d4dff34f49c55a2187b8e65168bda8806
FireFly0707 <jakubswietliczny@gmail.com>
2025-01-21 20:11:25 +0100

    Add new entities and relationships to AppDbContext
    - Added DbSet properties for Exercise, ExerciseTraining, ExerciseTrainingPlan, Set, Weight, Training, and TrainingPlan in AppDbContext.cs.
    - Overrode OnModelCreating to define:
      - Many-to-many relationship between Exercise and Training via ExerciseTraining.
      - Many-to-many relationship between Exercise and TrainingPlan via ExerciseTrainingPlan.
      - One-to-many relationship between Set and ExerciseTraining.
      - One-to-many relationship between Set and ExerciseTrainingPlan.
    - Added Weight.cs defining the Weight class with Id, Date, and Value properties.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MauiPocketTrainer.Model;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,54 @@ namespace MauiPocketTrainer.Data
         {
             
         }
-        
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<ExerciseTraining> ExerciseTrainings { get; set; }
+        public DbSet<ExerciseTrainingPlan> ExerciseTrainingPlans { get; set; }
+        public DbSet<Set> Sets { get; set; }
+        public DbSet<Weight> Weights { get; set; }
+        public DbSet<Training> Trainings { get; set; }
+        public DbSet<TrainingPlan> TrainingPlans { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Define relationships
+            modelBuilder.Entity<ExerciseTraining>()
+                .HasKey(et => new { et.ExerciseId, et.TrainingId });
+
+            modelBuilder.Entity<ExerciseTraining>()
+                .HasOne(et => et.Exercise)
+                .WithMany(e => e.ExerciseTrainings)
+                .HasForeignKey(et => et.ExerciseId);
+
+            modelBuilder.Entity<ExerciseTraining>()
+                .HasOne(et => et.Training)
+                .WithMany(t => t.ExerciseTrainings)
+                .HasForeignKey(et => et.TrainingId);
+
+            modelBuilder.Entity<ExerciseTrainingPlan>()
+                .HasKey(etp => new { etp.ExerciseId, etp.TrainingPlanId });
+
+            modelBuilder.Entity<ExerciseTrainingPlan>()
+                .HasOne(etp => etp.Exercise)
+                .WithMany(e => e.ExerciseTrainingPlans)
+                .HasForeignKey(etp => etp.ExerciseId);
+
+            modelBuilder.Entity<ExerciseTrainingPlan>()
+                .HasOne(etp => etp.TrainingPlan)
+                .WithMany(tp => tp.ExerciseTrainingPlans)
+                .HasForeignKey(etp => etp.TrainingPlanId);
+
+            modelBuilder.Entity<Set>()
+                .HasOne(s => s.ExerciseTraining)
+                .WithMany(et => et.Sets)
+                .HasForeignKey(s => s.ExerciseTrainingId);
+
+            modelBuilder.Entity<Set>()
+                .HasOne(s => s.ExerciseTrainingPlan)
+                .WithMany(etp => etp.Sets)
+                .HasForeignKey(s => s.ExerciseTrainingPlanId);
+        }
     }
 }
+
